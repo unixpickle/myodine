d0e3c5df44a6204c3599b483945cd98671a46231
Alex Nichol <unixpickle@gmail.com>
2018-03-21 21:20:55 -0700

    flesh out types for records

--- src/dns_proto/record.rs
+++ src/dns_proto/record.rs
@@ -1,3 +1,7 @@
+use std::net::{Ipv4Addr, Ipv6Addr};
+
+use dns_proto::domain::Domain;
+
 pub enum RecordType {
     A,
     NS,
@@ -6,9 +10,36 @@ pub enum RecordType {
     PTR,
     MX,
     TXT,
-    AAAA
+    AAAA,
+    Unknown(u16)
 }
 
 pub enum RecordClass {
-    IN
+    IN,
+    Unknown(u16)
+}
+
+pub struct RecordHeader {
+    pub domain: Domain,
+    pub record_type: RecordType,
+    pub record_class: RecordClass,
+    pub ttl: u32
+}
+
+pub struct SOADetails {
+    pub master_name: Domain,
+    pub responsible_name: Domain,
+    pub serial: u32,
+    pub refresh: u32,
+    pub retry: u32,
+    pub expire: u32,
+    pub minimum: u32
+}
+
+pub enum Record {
+    ARecord(RecordHeader, Ipv4Addr),
+    AAAARecord(RecordHeader, Ipv6Addr),
+    DomainRecord(RecordHeader, Domain),
+    SOARecord(RecordHeader, SOADetails),
+    Unknown(RecordHeader, Vec<u8>)
 }
