f52b0fa30c4861a0f555be0444baadb20a4ebcf5
Alex Nichol <unixpickle@gmail.com>
2018-03-24 16:03:14 -0700

    simplify NameCode API

--- src/myo_proto/name_code.rs
+++ src/myo_proto/name_code.rs
@@ -5,8 +5,27 @@ use dns_proto::domain::Domain;
 use super::util::domain_ends_with;
 
 pub trait NameCode {
-    fn name_encode(&self, root: &Domain, api_flag: char, data: &[u8]) -> Result<Domain, String>;
-    fn name_decode(&self, root: &Domain, encoded: &Domain) -> Result<(char, Vec<u8>), String>;
+    fn encode_parts(&self, api_flag: char, data: &[u8]) -> Result<Vec<String>, String>;
+    fn decode_parts(&self, parts: &[String]) -> Result<(char, Vec<u8>), String>;
+
+    fn encode_domain(&self, api_flag: char, data: &[u8], host: &Domain) -> Result<Domain, String> {
+        let mut parts = self.encode_parts(api_flag, data)?;
+        for part in host.parts() {
+            parts.push(part.clone());
+        }
+        Domain::from_parts(parts)
+    }
+
+    fn decode_domain(&self, name: &Domain, host: &Domain) -> Result<(char, Vec<u8>), String> {
+        if !domain_ends_with(name, host) {
+            Err(String::from("incorrect host domain"))
+        } else if name.parts().len() == host.parts().len() {
+            Err(String::from("no data in domain"))
+        } else {
+            let all_parts = name.parts().to_vec();
+            self.decode_parts(&all_parts[0..(name.parts().len() - host.parts().len())])
+        }
+    }
 }
 
 pub fn get_name_code(name: &str) -> Option<Box<NameCode>> {
@@ -19,7 +38,7 @@ pub fn get_name_code(name: &str) -> Option<Box<NameCode>> {
 pub struct HexNameCode;
 
 impl NameCode for HexNameCode {
-    fn name_encode(&self, root: &Domain, api_flag: char, data: &[u8]) -> Result<Domain, String> {
+    fn encode_parts(&self, api_flag: char, data: &[u8]) -> Result<Vec<String>, String> {
         let mut labels = Vec::new();
         let mut cur_label = format!("{}", api_flag);
         assert!(cur_label.is_ascii());
@@ -36,24 +55,14 @@ impl NameCode for HexNameCode {
         if cur_label.len() > 0 {
             labels.push(cur_label);
         }
-        for label in root.parts() {
-            labels.push(label.clone());
-        }
-        Domain::from_parts(labels)
+        Ok(labels)
     }
 
-    fn name_decode(&self, root: &Domain, encoded: &Domain) -> Result<(char, Vec<u8>), String> {
-        if encoded.parts().len() <= root.parts().len() {
-            return Err(String::from("no room for encoded data"));
-        }
-        let num_data_parts = encoded.parts().len() - root.parts().len();
-        if !domain_ends_with(encoded, root) {
-            return Err(String::from("incorrect root domain"));
-        }
-        let api_flag = encoded.parts()[0].as_bytes()[0] as char;
+    fn decode_parts(&self, parts: &[String]) -> Result<(char, Vec<u8>), String> {
+        let api_flag = parts[0].chars().next().unwrap();
         let mut data = Vec::new();
-        for i in 0..num_data_parts {
-            let label_bytes = encoded.parts()[i].as_bytes();
+        for (i, part) in parts.iter().enumerate() {
+            let label_bytes = part.as_bytes();
             if label_bytes.len() % 2 != 1 {
                 return Err(String::from("invalid label length"));
             }
@@ -82,9 +91,9 @@ mod tests {
         let root: Domain = "hello.com".parse().unwrap();
         let api_flag = 't';
         let code = HexNameCode{};
-        let encoded = code.name_encode(&root, api_flag, &vec![0u8, 1u8, 2u8, 52u8]).unwrap();
+        let encoded = code.encode_domain(api_flag, &vec![0u8, 1u8, 2u8, 52u8], &root).unwrap();
         assert_eq!(encoded, "t00010234.hello.com".parse().unwrap());
-        let (flag_out, decoded) = code.name_decode(&root, &encoded).unwrap();
+        let (flag_out, decoded) = code.decode_domain(&encoded, &root).unwrap();
         assert_eq!(flag_out, api_flag);
         assert_eq!(decoded, vec![0u8, 1u8, 2u8, 52u8]);
     }
@@ -102,11 +111,11 @@ mod tests {
                         0x26u8, 0xB6u8, 0x32u8, 0x37u8, 0xAAu8, 0xE5u8, 0xCCu8, 0x6Du8,
                         0x15u8, 0x08u8, 0xEAu8, 0xDDu8, 0x33u8, 0xADu8, 0xB1u8, 0x00u8,
                         0xEFu8, 0x01u8, 0x0Cu8, 0x71u8, 0xA2u8, 0x85u8];
-        let encoded = code.name_encode(&root, api_flag, &data).unwrap();
+        let encoded = code.encode_domain(api_flag, &data, &root).unwrap();
         assert_eq!(encoded, format!("{}.{}.foo.apple.com",
             "t234975ce6b0c717b8c1c4d4dcc259c0f00eeff057f7eb420bb04e385236329",
             "h99c461b4f00ef000a926b63237aae5cc6d1508eadd33adb100ef010c71a285").parse().unwrap());
-        let (flag_out, decoded) = code.name_decode(&root, &encoded).unwrap();
+        let (flag_out, decoded) = code.decode_domain(&encoded, &root).unwrap();
         assert_eq!(flag_out, api_flag);
         assert_eq!(decoded, data);
     }
