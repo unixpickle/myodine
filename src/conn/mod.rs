eef0abac6282be6cfe4a626e6a5211b757d83705
Alex Nichol <unixpickle@gmail.com>
2018-04-08 19:42:28 -0400

    move UDP highway to its own file

--- src/conn/mod.rs
+++ src/conn/mod.rs
@@ -3,7 +3,9 @@
 mod chunker;
 mod dial;
 mod highway;
+mod highway_udp;
 
 pub use self::chunker::TcpChunker;
 pub use self::dial::dial_udp;
-pub use self::highway::{Event, Highway, UDPHighway};
+pub use self::highway::{Event, Highway};
+pub use self::highway_udp::UDPHighway;
